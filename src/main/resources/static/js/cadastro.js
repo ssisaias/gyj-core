15336360ab6efc7c63c727c27c692c03ada1b8a2
Isaias S. Silva <luiz.isaias95@gmail.com>
2016-07-10 22:33:10 -0300

    Atualiza cadastro e home

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -4,14 +4,19 @@ window.onload = function(){
 	var senha = document.getElementById("senha");
 	var conf = document.getElementById("conf-senha");
 	var cadastrar = document.getElementById("cadastrar");
-	var span = document.getElementById("span");
+	var spanCampo = document.getElementById("campos");
+	var spanSenha = document.getElementById("confirmarSenha");
 
 	cadastrar.addEventListener('click',campoVazio,false);
 	
 	function campoVazio(event){
 		if(nome.value.length == 0 || email.value.length == 0 || senha.value.length == 0 || conf.value.length == 0){
 			event.preventDefault();
-			span.innerHTML = "Campo obrigatório em branco, por favor preencher.";
+			spanCampo.innerHTML = "Campos obrigatórios em branco, por favor preencher.";
+		}
+		if(senha.value != conf.value){
+			event.preventDefault();
+			spanSenha.innerHTML = "A senha e a Confirmação da senha devem ser iguais.";
 		}
 	}
 	
