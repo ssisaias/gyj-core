bc4cd216f9991746b4ed7e454a92de2f3e1f1d00
Isaias S. Silva <luiz.isaias95@gmail.com>
2016-07-11 00:21:58 -0300

    Cria view cadastroAcademia e atualiza javascript

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -1,16 +1,16 @@
 window.onload = function(){
-	var nome = document.getElementById("nome");
-	var email = document.getElementById("email");
+	var nomeUser = document.getElementById("nome");
+	var emailUser = document.getElementById("email");
 	var senha = document.getElementById("senha");
 	var conf = document.getElementById("conf-senha");
-	var cadastrar = document.getElementById("cadastrar");
+	var cadastrarUser = document.getElementById("cadastrar");
 	var spanCampo = document.getElementById("campos");
 	var spanSenha = document.getElementById("confirmarSenha");
 
-	cadastrar.addEventListener('click',campoVazio,false);
+	cadastrarUser.addEventListener('click',campoVazioUser,false);
 	
-	function campoVazio(event){
-		if(nome.value.length == 0 || email.value.length == 0 || senha.value.length == 0 || conf.value.length == 0){
+	function campoVazioUser(event){
+		if(nomeUser.value.length == 0 || emailUser.value.length == 0 || senha.value.length == 0 || conf.value.length == 0){
 			event.preventDefault();
 			spanCampo.innerHTML = "Campos obrigatórios em branco, por favor preencher.";
 		}
@@ -20,4 +20,20 @@ window.onload = function(){
 		}
 	}
 	
+	var nomeAcademia = document.getElementById("nomeAcademia");
+	var enderecoAcademia = document.getElementById("enderecoAcademia");
+	var cidadeAcademia = document.getElementById("cidadeAcademia");
+	var emailAcademia = document.getElementById("emailAcademia");
+	var telefoneAcademia = document.getElementById("telefoneAcademia");
+	var cadastrarAcademia = document.getElementById("cadastrarAcademia");
+	var spanAcademia = document.getElementById("camposAcademia");
+	
+	cadastrarAcademia.addEventListener('click',campoVazioAcademia,false);
+	
+	function campoVazioAcademia(event){
+		if(nomeAcademia.value.length == 0 || enderecoAcademia.value.length == 0 || cidadeAcademia.value.length == 0){
+			event.preventDefault();
+			spanAcademia.innerHTML = "Campos obrigatórios em branco, por favor preencher.";
+		}
+	}
 }
