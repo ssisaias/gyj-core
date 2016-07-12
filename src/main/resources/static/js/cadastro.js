67c80a4751bf60beee08d9f7530122fcd8b413a7
aadleh <leticia.aad@gmail.com>
2016-07-12 09:25:17 -0300

    Atualiza layout Cadastro, Login, Academias e CadastroAcademia, Cria view teste

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -4,18 +4,20 @@ window.onload = function(){
 	var senha = document.getElementById("senha");
 	var conf = document.getElementById("conf-senha");
 	var cadastrarUser = document.getElementById("cadastrar");
-	var spanCampo = document.getElementById("campos");
-	var spanSenha = document.getElementById("confirmarSenha");
+	var spanCampo = document.getElementById("errorCampos");
+	var spanSenha = document.getElementById("errorSenha");
 
 	cadastrarUser.addEventListener('click',campoVazioUser,false);
 	
 	function campoVazioUser(event){
 		if(nomeUser.value.length == 0 || emailUser.value.length == 0 || senha.value.length == 0 || conf.value.length == 0){
 			event.preventDefault();
+			spanCampo.style.color = "red";
 			spanCampo.innerHTML = "Campos obrigatórios em branco, por favor preencher.";
 		}
 		if(senha.value != conf.value){
 			event.preventDefault();
+			spanSenha.style.color = "red";
 			spanSenha.innerHTML = "A senha e a Confirmação da senha devem ser iguais.";
 		}
 	}
